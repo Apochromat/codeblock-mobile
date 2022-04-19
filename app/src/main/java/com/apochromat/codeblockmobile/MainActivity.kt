1c03e5b32a2fb360118beef33975c7afabdaceee
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-04-19 10:46:57 +0700

    Initial project

---
+++ app/src/main/java/com/apochromat/codeblockmobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.apochromat.codeblockmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
