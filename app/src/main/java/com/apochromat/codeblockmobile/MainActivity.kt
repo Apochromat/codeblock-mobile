386d26f93802086db444dc2d72c2e7eba160ed9e
Alexander <podsolnyx03@mail.ru>
2022-04-27 23:35:10 +0700

    Дизайн эволюшен

--- app/src/main/java/com/apochromat/codeblockmobile/MainActivity.kt
+++ app/src/main/java/com/apochromat/codeblockmobile/MainActivity.kt
@@ -3,6 +3,7 @@ package com.apochromat.codeblockmobile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.apochromat.codeblockmobile.databinding.ActivityMainBinding
 import kotlin.system.exitProcess
 
@@ -10,19 +11,25 @@ class MainActivity : AppCompatActivity() {
     lateinit var bindingClass : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
         supportActionBar?.title = ""
-        bindingClass.buttonCreateNewProject.setOnClickListener{
-            val intent = Intent(this@MainActivity, ProjectActivity::class.java)
-            startActivity(intent)
-        }
-        bindingClass.buttonAboutAuthors.setOnClickListener{
-            val intent = Intent(this@MainActivity, AuthorsActivity::class.java)
-            startActivity(intent)
-        }
-        bindingClass.buttonExitApp.setOnClickListener{
-            finishAffinity()
+        init()
+    }
+    private fun init(){
+        bindingClass.apply{
+            buttonCreateNewProject.setOnClickListener{
+                val intent = Intent(this@MainActivity, ProjectActivity::class.java)
+                startActivity(intent)
+            }
+            buttonAboutAuthors.setOnClickListener{
+                val intent = Intent(this@MainActivity, AuthorsActivity::class.java)
+                startActivity(intent)
+            }
+            buttonExitApp.setOnClickListener{
+                finishAffinity()
+            }
         }
     }
 }
--- app/src/main/java/com/apochromat/codeblockmobile/RVAdaptor.kt
+++ app/src/main/java/com/apochromat/codeblockmobile/RVAdaptor.kt
@@ -1,5 +1,6 @@
 package com.apochromat.codeblockmobile
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,73 +12,25 @@ class RVAdaptor(private val listBlocks:ArrayList<Block>) : RecyclerView.Adapter<
         val textType: TextView = itemView.findViewById(R.id.text1)
         val textStatus: TextView = itemView.findViewById(R.id.text3)
     }
-    
-    fun getViewTypeCount(): Int {
-        return 8
-    }
 
     override fun getItemViewType(position: Int): Int {
         val viewType = when(listBlocks[position].getBlockType()){
-            "UndefinedVariable" -> 1
-            "Assignment" -> 2
-            "ConditionIf" -> 3
-            "ConditionIfElse" -> 4
-            "CycleWhile" -> 5
-            "ConsoleOutput" -> 6
-            "ConsoleInputOne" -> 7
-            else -> 0
+            "UndefinedVariable" -> R.layout.item_undefined_var
+            "Assignment" -> R.layout.item_assignment
+            "ConditionIf" -> R.layout.item_condition_if
+            "ConditionIfElse" -> R.layout.item_condition_if_else
+            "CycleWhile" -> R.layout.item_cycle_while
+            "ConsoleOutput" -> R.layout.item_console_output
+            "ConsoleInputOne" -> R.layout.item_console_input_one
+            else -> R.layout.item_defined_var
         }
         return viewType;
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return when (viewType) {
-            0 -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.defined_var_rv, parent, false)
-                ViewHolder(view)
-            }
-            1 -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.undefined_var_rv, parent, false)
-                ViewHolder(view)
-            }
-            2 -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.assignment_rv, parent, false)
-                ViewHolder(view)
-            }
-            3 -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.condition_if_rv, parent, false)
-                ViewHolder(view)
-            }
-            4 -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.condition_if_else_rv, parent, false)
-                ViewHolder(view)
-            }
-            5 -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.cycle_while_rv, parent, false)
-                ViewHolder(view)
-            }
-            6 -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.console_output_rv, parent, false)
-                ViewHolder(view)
-            }
-            7 -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.console_input_one_rv, parent, false)
-                ViewHolder(view)
-            }
-            else -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.defined_var_rv, parent, false)
-                ViewHolder(view)
-            }
-        }
+        val view = LayoutInflater.from(parent.context)
+            .inflate(viewType, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -88,6 +41,7 @@ class RVAdaptor(private val listBlocks:ArrayList<Block>) : RecyclerView.Adapter<
     override fun getItemCount(): Int {
         return listBlocks.size
     }
+    @SuppressLint("NotifyDataSetChanged")
     fun addBlock(block : Block){
         listBlocks.add(block)
         notifyDataSetChanged()
