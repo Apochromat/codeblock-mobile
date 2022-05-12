bcba62fdf69ff91eba12035f40a0c8ae53e5079b
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-05-12 20:54:39 +0700

    штуки

--- app/src/main/java/com/apochromat/codeblockmobile/logic/MainLogic.kt
+++ app/src/main/java/com/apochromat/codeblockmobile/logic/MainLogic.kt
@@ -1,21 +1,31 @@
 package com.apochromat.codeblockmobile.logic
-
 fun main() {
     val ep = EntryPoint()
     val da = UndefinedArray()
     val uv = DefinedVariable()
-    val dv=DefinedVariable()
     val vass = Assignment()
     val ass = Assignment()
     val wh = CycleWhile()
     val out = ConsoleOutput()
     val vout = ConsoleOutput()
-    dv.setBlockInput("var","1")
-    Block().accessHeap().createArray("Pupsich", 1337)
-    Block().accessHeap().setArrayValue("Pupsich",1,5)
-    connectBlocks(ep,dv)
-    connectBlocks(dv,out)
+
+    da.setBlockInput("arr", "5")
+    out.setBlockInput("arr[5]")
+    vout.setBlockInput("var")
+    uv.setBlockInput("var", "0")
+    vass.setBlockInput("var", "var+1")
+    ass.setBlockInput("arr[var]", "var")
+    wh.setBlockInput("var", "2", "<")
+
+    connectBlocks(ep, da)
+    connectBlocks(da, uv)
+    connectBlocks(uv, wh)
+    connectBlocks(wh.begin, vass)
+    connectBlocks(vass, ass)
+    connectBlocks(ass, vout)
+    connectBlocks(vout, wh.end)
+    connectBlocks(wh, out)
+
     ep.run()
-    println(Block().accessHeap().isVariableExist("var"))
-    println(defineInput(Block().accessHeap(),"var"))
+    println(ep.accessHeap().getArrayValue("arr", 2))
 }
